fbd723455264b3832d3fbbe2f5dd643deb845719
luismateus <luis.afonso.mateus@gmail.com>
2018-05-30 17:36:16 +0100

    main changes

--- AmbidexBot/AmbidexMain.py
+++ AmbidexBot/AmbidexMain.py
@@ -9,14 +9,32 @@ from Status import Status
 def main():
     playerArray = []
     game = GameInstance()
-    if(game.createGame(game, "main", "object?")): #o que e o playerobject?
+    if(game.createGame("main", "object?")): #o que e o playerobject?
         for i in range(0,9):
-            machine = game.addMachine(game)
+            machine = game.addMachine()
             playerArray.append(machine)
-    game.startGame(game)
+    game.startGame()
     while(game.checkInProgress):
         #bots recebem as combinacoes e teem de votar numa. HOW? criar array de consideracoes para cada player
-        # -> fazer funcao que calcula as combinacoes possiveis e a "utilidade" para cada player para que estes votem
-        # -> fazer funcao que receba a votacao dos players e devolva a combinacao possivel ou recomece a votacao em caso de empates
+        if(not(game.ActivePolling or game.LockAmbidex)): # time for votation, falta verificacao para ver se os players nao estao dentro das portas
+            game.clearCombi()
+            for player in playerArray:
+                game.setPlayerCombi(player)
+            game.ActivePolling = True
+            combiA = game.combinations["a"]
+            combiB = game.combinations["b"]
+            combiC = game.combinations["c"]            
+            #HERE : put combinations in log
+            #calcular a utilidade para cada player de acordo com os consideration values e retornar combinacao escolhida "por todos"
+            #game.setPlayerDoors(combi) // combi returnada na linha anterior
+            game.ActivePolling=False
+            game.LockAmbidex=True
+        if(game.LockAmbidex and (not game.AmbidexInProgress)):
+            game.AmbidexInProgress=True
+            #negotiation between pairs, solo vote is linear 
+            
+            
+            
 
-    
+
+
